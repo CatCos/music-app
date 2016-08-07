5699b2f9d50e95f4776ef3977f87b5368e46ffa8
CatCos <catarinacosta217@gmail.com>
2016-08-07 23:14:15 +0100

    Change port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = new Hapi.Server();
 
 server.connection({
     host: process.env.HOST,
-    port: process.env.PORT
+    port: 8000
 });
 
 server.register([require('hapi-auth-cookie'),
