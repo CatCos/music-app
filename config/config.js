38e5b6a3aa5217d0d5019b3a3bcaed79a9c18e9d
CatCos <catarinacosta217@gmail.com>
2016-08-07 23:06:15 +0100

    Add missing env var

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "musikki",
+    "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
     "seederStorage": "sequelize"
@@ -10,14 +10,14 @@ module.exports = {
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "musikki",
+    "database":process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "musikki",
+    "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   }
